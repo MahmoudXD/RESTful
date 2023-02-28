68ca4cc77bb3e8ae3097c7e555e314e8408b386b
mahmoudxd <mahmoudsherbenyxd@gmail.com>
2023-02-28 10:14:40 +0300

    First Commit

---
+++ Course/Controllers/RootController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Controllers
+{
+	[Route("/")]
+	[ApiController]
+	public class RootController: ControllerBase
+	{
+		[HttpGet(Name = nameof(GetRoot))]
+		public IActionResult GetRoot()
+		{
+			var respone = new
+			{
+				href = Url.Link(nameof(GetRoot), null)
+			};
+
+			return Ok(respone);
+		}
+	}
+}
+
